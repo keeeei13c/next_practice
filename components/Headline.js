0f51642c979dad1e4718c6d9afc514e1ee4a0d1c
keeeei13c <kei.121003f@icloud.com>
2021-04-13 20:25:30 +0900

    ♻️ メイン部分のコンポーネント化

--- components/Headline.js
+++ components/Headline.js
@@ -5,11 +5,12 @@ export function Headline(props) {
     <div>
       <h1 className={styles.title}>
        {props.page} Page
-        </h1>
+      </h1>
       <p className={styles.description}>
-        Get started by editing{' '}
-        <code className={styles.code}>pages/{ props.page}.js</code>
+        Get started by editing {props.children}
       </p>
+
+      <button onClick={props.onClick}>button</button>
     </div>
   );
 }
