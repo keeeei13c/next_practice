081a43b88d243d010849fc586e5826001218dca4
keeeei13c <kei.121003f@icloud.com>
2021-04-14 12:12:50 +0900

    ✨ ヘッダーの追加

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
-import {Main} from '../components/Main'
+import { Main } from '../components/Main'
+import {Header} from '../components/Header'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <title>index page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <Main page="index" />
       <Footer />
     </div>
