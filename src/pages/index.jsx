cf253d664a0622742d0d0a21c508159df8e89e8a
keeeei13c <kei.121003f@icloud.com>
2021-06-25 16:43:16 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,23 +3,19 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Main } from 'src/components/Main'
 import {Header} from 'src/components/Header'
-import { useEffect} from 'react'
+import { useEffect,useState} from 'react'
   
 export default function Home() {
-
-  // const foo = 1;
-  //  const handleClick = useCallback((e) => {
-  //   console.log(e.target.href);
-  //   e.preventDefault();
-  //   alert(foo)
-  //  }, []);
+  const [count, setCount] = useState(1);
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+    setCount((count) => count + 1);
+  };
   
   useEffect(() => {
     document.body.style.background = "lightblue";
-    console.log("マウント時");
     return () => {
       document.body.style.background = "beige";
-      console.log("アンマウント時")
     };
   }, []);
 
@@ -29,12 +25,8 @@ export default function Home() {
         <title>index page</title>
       </Head>
       <Header />
-      <a
-        href="/about"
-        // onClick={handleClick}
-      >
-        button
-      </a>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
